8b5772095cdb731c642de4d130d94691d3a32ee0
Rohit8510 <83526517+Rohit8510@users.noreply.github.com>
2026-07-01 08:40:14 +0530

    Update database connection method
    Refactored database connection to use environment variables.

--- conn.php
+++ conn.php
@@ -1,12 +1,7 @@
-<?php
-$servername = "localhost";
-$username = "your_usernamr";
-$password = "your_password";
-$dbname = "your_db";
-
-$conn = mysqli_connect($servername,$username,$password,$dbname);
-
-if (!$conn) {
-    die(json_encode(['status' => 'fail', 'reason' => 'Database connection failed']));
-}
-?>
+$conn = mysqli_connect(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
